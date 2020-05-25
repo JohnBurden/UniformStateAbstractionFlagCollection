5f8fd0a39ff9c2f1fe2cf2017c70875d76c5c167
JohnBurden <kionairis@gmail.com>
2020-05-25 15:23:00 +0100

    Removed Excess Code

--- main.py
+++ main.py
@@ -103,28 +103,6 @@ for r in range(0,numRepetitions):
 		print("Begin Training:")
 		for ep in range(0,numEpisodes):
 
-			#if ep == numEpisodes/2:
-				#pass
-				#collectingReachability = False
-				#room = agent.abstraction()[e]
-				#roomBool = copy.deepcopy(room)
-				#for y in range(0, len(roomBool)):
-			#		for x in range(0, len(roomBool[y])):
-		#				roomBool[y][x] = False
-		#		room.reverse()
-		#		for ab in range(0, len(agent.reachabilityBins)):
-		#			for biN in range(1, len(agent.reachabilityBins[ab])):
-		#				for st in range(0, len(agent.reachabilityBins[ab][biN])):
-		#					label = str(ab)+"-"+str(biN)
-		#					s = agent.reachabilityBins[ab][biN][st]
-		#					if not roomBool[int(s[1])][int(s[0])]:
-		#						room[int(s[1])][int(s[0])] = label
-		#						roomBool[int(s[1])][int(s[0])]=True
-		#		room.reverse()
-				#if e == 1:
-					#roomR = room
-				#abstraction=QLambdaAbstractGen.Abstraction.Abstraction(room, env.flags, env.goal, env.state)
-				#abstraction.solveAbstraction()
 
 			episodeReward = 0
 			if ep%100==0:
@@ -148,59 +126,8 @@ for r in range(0,numRepetitions):
 				newAbstractState = abstraction.abstractState(newState)
 #				print(newState, newAbstractState) 
 
-				abstractBinned = False
-				stateBinnedi = -1
-				prevStateBinnedi = -1
-				stateBinnedj = -1
-				prevStateBinnedj = -1
 
-				#if collectingReachability:
 
-				#	for i in range(0, len(agent.reachabilityBins)):
-				#		if agent.reachabilityBins[i][0] == abstractState:
-				#			prevStateBinnedi = i
-				#	## Find which bin the previous state is in
-#
-#					for i in range(0, len(agent.reachabilityBins)):
-				#		if agent.reachabilityBins[i][0] == newAbstractState:
-				#			stateBinnedi = i
-				#		## Find which bin new state is in
-				#			for j in range(1, len(agent.reachabilityBins[i])):
-				#				if newState in agent.reachabilityBins[i][j]:
-				#					stateBinnedj = j
-				#				if env.state in agent.reachabilityBins[i][j]:
-				#					prevStateBinnedj = j
-				#				## Find which sub-bin each state is in.
-#
-				#			if stateBinnedj == -1:
-				#				if abstractState == newAbstractState:
-				#					if ep > 5000:
-				#						pass
-				#						#print(newState)
-				#						#print(prevStateBinnedj)
-				#						#print(agent.reachabilityBins)
-				#						#print(agent.reachabilityBins[i])
-				#					agent.reachabilityBins[i][prevStateBinnedj].append(newState)
-				#				else:
-				#					agent.reachabilityBins[i].append([newState])
-				#				## If new state has no sub-bin, either put it in previous state's sub-bin
-				#				## Or make a new sub-bin for it.
-				#			else:
-				#				if stateBinnedi == prevStateBinnedi:
-				#					if not prevStateBinnedj == stateBinnedj:
-				#						agent.reachabilityBins[i][prevStateBinnedj].extend(agent.reachabilityBins[i][stateBinnedj])
-				#						del agent.reachabilityBins[i][stateBinnedj]
-				#				## If the two states share the same bin, but not sub-bin,
-				#				## Then merge the two.
-#
-#					if stateBinnedi == -1:
-#						agent.reachabilityBins.append([newAbstractState])
-#						agent.reachabilityBins[-1].append([newState])
-#						## If the new state is in no bin, then create a new bin for it
-
-			
-			#	if (agent.roomObservations[newState[0]][newState[1]] == -1).all():
-			#		agent.roomObservations[newState[0]][newState[1]] = agent.generateStateObservation(newState)
 				path.append(newState)
 
 
@@ -264,11 +191,6 @@ def mkdir_p(mypath):
 ################## Draw Graph ########################
 
 
-#print(flagCount)#
-#print(flagCount/numEpisodes)
-#print(totalMoveCount/numEpisodes)
-
-
 labs = ["True", "3x3 Tiling","4x4 Tiling", "5x5 Tiling", "7x7 Tiling", "9x9 Tiling", "10x10 Tiling", "No Shaping"]
 labs = ["True", "9x9 Tiling", "12x12 Tiling", "15x15 Tiling", "21x21 Tiling", "27x27 Tiling", "32x24 Tiling", "No Shaping"]
 
@@ -295,11 +217,7 @@ for i in range(0, len(plotRewards)):
 	d = pd.Series(plotRewards[i])
 	s = pd.Series(plotErrors[i])
 	movAv=pd.Series.rolling(d,window=int(numEpisodes/10), center=False).mean()
-	#movStd=pd.Series.rolling(s,window=1000, center=False).mean()
-#	for j in range(0,len(movAv)):
-#		if movAv[j] > 2.9:
-#			whenConverged.append((i,j))
-#			break
+
 	toPickle.append(movAv)	
 	l, caps, c = plt.errorbar(np.arange(len(movAv)),movAv, label=labs[i], yerr=plotErrors[i], capsize=5, errorevery=numEpisodes/10)
 	for cap in caps:
